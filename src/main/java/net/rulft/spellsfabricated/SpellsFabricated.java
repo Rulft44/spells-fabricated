7f43c9a0aab737193ced1c236d26bbf14ee8c5eb
Rulft44 <68545737+Rulft44@users.noreply.github.com>
2024-09-07 12:01:31 +0100

    :tada: First commit

---
+++ src/main/java/net/rulft/spellsfabricated/SpellsFabricated.java
@@ -0,0 +1,17 @@
+package net.rulft.spellsfabricated;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpellsFabricated implements ModInitializer {
+	public static final String MOD_ID = "spellsfabricated";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Erm 🤓");
+	}
+}
---
+++ src/main/java/net/rulft/spellsfabricated/SpellsFabricatedClient.java
@@ -0,0 +1,10 @@
+package net.rulft.spellsfabricated;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpellsFabricatedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
